765d8f46a61ec6e6879f3edd6421905bc8a503a8
KateKrivikh <62333408+katekrivikh@users.noreply.github.com>
2020-05-13 02:10:18 +0300

    Применила Given-When-Than. Наименование тестов задано в формате TestMethod_Condition_ExpectedResult.

--- src/test/java/com/education/controller/command/PersonCommandFactoryTest.java
+++ src/test/java/com/education/controller/command/PersonCommandFactoryTest.java
@@ -6,37 +6,63 @@ import org.junit.Test;
 
 public class PersonCommandFactoryTest {
 
+    Operation operation;
+    PersonCommand command;
+
     @Test
-    public void createAdd() {
-        PersonCommand actual = PersonCommandFactory.create(Operation.ADD);
-        PersonCommand expected = new PersonCommandAdd();
-        Assert.assertEquals(expected, actual);
+    public void create_AddOperation_AddCommand() {
+        Given:
+        operation = Operation.ADD;
+
+        When:
+        command = PersonCommandFactory.create(operation);
+
+        Then:
+        Assert.assertTrue(command instanceof PersonCommandAdd);
     }
 
     @Test
-    public void createUpdate() {
-        PersonCommand actual = PersonCommandFactory.create(Operation.UPDATE);
-        PersonCommand expected = new PersonCommandUpdate();
-        Assert.assertEquals(expected, actual);
+    public void create_UpdateOperation_UpdateCommand() {
+        Given:
+        operation = Operation.UPDATE;
+
+        When:
+        command = PersonCommandFactory.create(operation);
+
+        Then:
+        Assert.assertTrue(command instanceof PersonCommandUpdate);
     }
 
     @Test
-    public void createRemove() {
-        PersonCommand actual = PersonCommandFactory.create(Operation.REMOVE);
-        PersonCommand expected = new PersonCommandRemove();
-        Assert.assertEquals(expected, actual);
+    public void create_RemoveOperation_RemoveCommand() {
+        Given:
+        operation = Operation.REMOVE;
+
+        When:
+        command = PersonCommandFactory.create(operation);
+
+        Then:
+        Assert.assertTrue(command instanceof PersonCommandRemove);
     }
 
     @Test
-    public void createInfo() {
-        PersonCommand actual = PersonCommandFactory.create(Operation.INFO);
-        PersonCommand expected = new PersonCommandInfo();
-        Assert.assertEquals(expected, actual);
+    public void create_InfoOperation_InfoCommand() {
+        Given:
+        operation = Operation.INFO;
+
+        When:
+        command = PersonCommandFactory.create(operation);
+
+        Then:
+        Assert.assertTrue(command instanceof PersonCommandInfo);
     }
 
     @Test(expected = OperationIsEmptyException.class)
-    public void createNull() {
-        Operation nullOperation = null;
-        PersonCommandFactory.create(nullOperation);
+    public void create_Null_Exception() {
+        Given:
+        operation = null;
+
+        When:
+        command = PersonCommandFactory.create(operation);
     }
 }
